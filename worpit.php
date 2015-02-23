854d73762eb6e897dac7495ff0a6c11d2e22b040
Paul Goodchild <paul@icontrolwp.com>
2015-02-23 12:46:38 +0000

    update version numbers fo 2.11.3 release.

--- worpit.php
+++ worpit.php
@@ -3,7 +3,7 @@
 Plugin Name: iControlWP
 Plugin URI: http://icwp.io/home
 Description: Take Control Of All WordPress Sites From A Single Dashboard
-Version: 2.11.2
+Version: 2.11.3
 Author: iControlWP
 Author URI: http://www.icontrolwp.com/
 */
