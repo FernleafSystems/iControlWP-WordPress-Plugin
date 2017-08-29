8d3899f28eed774d9ae822d200772b7fb7f208fb
Paul Goodchild <paul@icontrolwp.com>
2017-08-29 12:01:11 +0100

    improvements and fixes to data gathering and use of FTP details for updates

--- src/api/internal/collect/base.php
+++ src/api/internal/collect/base.php
@@ -1,18 +1,29 @@
 <?php
 
-if ( !class_exists( 'ICWP_APP_Api_Internal_Collect_Base', false ) ):
+if ( class_exists( 'ICWP_APP_Api_Internal_Collect_Base', false ) ) {
+	return;
+}
 
-	require_once( dirname( dirname( __FILE__ ) ).ICWP_DS.'base.php' );
+require_once( dirname( dirname( __FILE__ ) ) . ICWP_DS . 'base.php' );
 
-	class ICWP_APP_Api_Internal_Collect_Base extends ICWP_APP_Api_Internal_Base {
+class ICWP_APP_Api_Internal_Collect_Base extends ICWP_APP_Api_Internal_Base {
 
-		/**
-		 * @param string $sContext
-		 * @return mixed
-		 */
-		protected function getAutoUpdates( $sContext = 'plugins' ) {
-			return ICWP_Plugin::GetAutoUpdatesSystem()->getAutoUpdates( $sContext );
-		}
+	/**
+	 * @param bool $bFull
+	 * @return array
+	 */
+	protected function collectCapabilities( $bFull = false ) {
+		require_once( dirname( __FILE__ ) . ICWP_DS . 'capabilites.php' );
+		$oCollector = new ICWP_APP_Api_Internal_Collect_Capabilities();
+		$oCollector->setRequestParams( $this->getRequestParams() );
+		return $oCollector->collect( $bFull );
 	}
 
-endif;
+	/**
+	 * @param string $sContext
+	 * @return mixed
+	 */
+	protected function getAutoUpdates( $sContext = 'plugins' ) {
+		return ICWP_Plugin::GetAutoUpdatesSystem()->getAutoUpdates( $sContext );
+	}
+}
--- src/api/internal/collect/info.php
+++ src/api/internal/collect/info.php
@@ -1,49 +1,41 @@
 <?php
 
-if ( !class_exists( 'ICWP_APP_Api_Internal_Collect_Info', false ) ):
+if ( class_exists( 'ICWP_APP_Api_Internal_Collect_Info', false ) ) {
+	return;
+}
 
-	require_once( dirname( __FILE__ ).ICWP_DS.'base.php' );
+require_once( dirname( __FILE__ ) . ICWP_DS . 'base.php' );
 
-	class ICWP_APP_Api_Internal_Collect_Info extends ICWP_APP_Api_Internal_Collect_Base {
-		/**
-		 * @return ApiResponse
-		 */
-		public function process() {
-			$aData = array(
-				'capabilities' => $this->collectCapabilities(),
-				'wordpress-plugins' => $this->collectPlugins(),
-				'wordpress-themes' => $this->collectThemes(),
-				'wordpress-info' => $this->collectWordpress(),
-				'wordpress-extras' => array(
-					'preferred-core-update'	=> get_preferred_from_update_core()
-				),
-				'wordpress-paths' => array(
-					'wordpress_admin_url'	=> network_admin_url()
-				),
-				'force_update_check' => $this->isForceUpdateCheck() ? 1 : 0
-			);
-			return $this->success( $aData );
-		}
+class ICWP_APP_Api_Internal_Collect_Info extends ICWP_APP_Api_Internal_Collect_Base {
 
-		/**
-		 * @return array
-		 */
-		private function collectCapabilities() {
-			require_once( dirname( __FILE__ ).ICWP_DS.'capabilites.php' );
-			$oCollector = new ICWP_APP_Api_Internal_Collect_Capabilities();
-			$oCollector->setRequestParams( $this->getRequestParams() );
-			return $oCollector->collect();
-		}
+	/**
+	 * @return ApiResponse
+	 */
+	public function process() {
+		$aData = array(
+			'capabilities'       => $this->collectCapabilities( false ),
+			'wordpress-plugins'  => $this->collectPlugins(),
+			'wordpress-themes'   => $this->collectThemes(),
+			'wordpress-info'     => $this->collectWordpress(),
+			'wordpress-extras'   => array(
+				'preferred-core-update' => get_preferred_from_update_core()
+			),
+			'wordpress-paths'    => array(
+				'wordpress_admin_url' => network_admin_url()
+			),
+			'force_update_check' => $this->isForceUpdateCheck() ? 1 : 0
+		);
 
-		/**
-		 * @return array
-		 */
-		private function collectWordpress() {
-			require_once( dirname( __FILE__ ).ICWP_DS.'wordpress.php' );
-			$oCollector = new ICWP_APP_Api_Internal_Collect_Wordpress();
-			$oCollector->setRequestParams( $this->getRequestParams() );
-			return $oCollector->collect();
-		}
+		return $this->success( $aData );
 	}
 
-endif;
+	/**
+	 * @return array
+	 */
+	private function collectWordpress() {
+		require_once( dirname( __FILE__ ) . ICWP_DS . 'wordpress.php' );
+		$oCollector = new ICWP_APP_Api_Internal_Collect_Wordpress();
+		$oCollector->setRequestParams( $this->getRequestParams() );
+		return $oCollector->collect();
+	}
+}
