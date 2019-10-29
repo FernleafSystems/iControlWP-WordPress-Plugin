862d9a955bdf9bbf41e0703f466cb0c69c9dc3e5
Paul Goodchild <paul@icontrolwp.com>
2019-10-29 10:36:13 +0000

    reorganise google analytics

--- src/processors/google_analytics.php
+++ src/processors/google_analytics.php
@@ -7,11 +7,20 @@ class ICWP_APP_Processor_GoogleAnalytics extends ICWP_APP_Processor_BaseApp {
 	 */
 	private $aGaOptions;
 
-	/**
-	 */
 	public function run() {
+		add_action( 'wp', array( $this, 'onWp' ) );
+	}
+
+	public function onWp() {
 		$this->migrateOptions();
-		add_action( 'init', array( $this, 'onWpInit' ) );
+
+		$aOpts = $this->getGaOpts();
+		if ( !empty( $aOpts[ 'tracking_id' ] ) && !$this->getIfIgnoreUser() ) {
+			add_action( $this->getWpHook(), array( $this, 'doPrintGoogleAnalytics' ), 100 );
+			if ( $this->getAnalyticsMode() == 'tags' ) {
+				add_action( 'wp_body_open', array( $this, 'printTagsBody' ) );
+			}
+		}
 	}
 
 	/**
@@ -24,28 +33,6 @@ class ICWP_APP_Processor_GoogleAnalytics extends ICWP_APP_Processor_BaseApp {
 				'analytics_mode',
 				( $oMod->getOpt( 'enable_universal_analytics' ) == 'Y' ) ? 'universal' : 'classic'
 			);
-		}
-	}
-
-	public function onWpInit() {
-		$oMod = $this->getFeatureOptions();
-		$sTrackingId = $oMod->getOpt( 'tracking_id' );
-		if ( !empty( $sTrackingId ) && !$this->getIfIgnoreUser() ) {
-			$this->aGaOptions = array(
-				'tracking_id'            => $sTrackingId,
-				'analytics_mode'         => $oMod->getOpt( 'analytics_mode' ),
-				'ignore_logged_in_user'  => $oMod->getOpt( 'ignore_logged_in_user' ),
-				'ignore_from_user_level' => $oMod->getOpt( 'ignore_from_user_level', 11 ),
-				'in_footer'              => $oMod->getOpt( 'in_footer' ),
-			);
-			add_action( 'wp', array( $this, 'onWp' ) );
-		}
-	}
-
-	public function onWp() {
-		add_action( $this->getWpHook(), array( $this, 'doPrintGoogleAnalytics' ), 100 );
-		if ( $this->getAnalyticsMode() == 'tags' ) {
-			add_action( 'wp_body_open', array( $this, 'printTagsBody' ) );
 		}
 	}
 
@@ -80,23 +67,35 @@ class ICWP_APP_Processor_GoogleAnalytics extends ICWP_APP_Processor_BaseApp {
 	 * @return array
 	 */
 	private function getGaOpts() {
-		return is_array( $this->aGaOptions ) ? $this->aGaOptions : array();
+		$oMod = $this->getFeatureOptions();
+		if ( empty( $this->aGaOptions ) ) {
+			$this->aGaOptions = array(
+				'tracking_id'            => $oMod->getOpt( 'tracking_id' ),
+				'analytics_mode'         => $oMod->getOpt( 'analytics_mode' ),
+				'ignore_logged_in_user'  => $oMod->getOpt( 'ignore_logged_in_user' ),
+				'ignore_from_user_level' => $oMod->getOpt( 'ignore_from_user_level', 11 ),
+				'in_footer'              => $oMod->getOpt( 'in_footer' ),
+			);
+		}
+		return $this->aGaOptions;
 	}
 
 	/**
 	 * @return bool
 	 */
 	private function getIfIgnoreUser() {
+		$bIgnore = false;
+
 		$aOpts = $this->getGaOpts();
-		$bIgnoreLoggedInUser = $aOpts[ 'ignore_logged_in_user' ] == 'Y';
 		$nCurrentUserLevel = $this->loadWpUsersProcessor()->getCurrentUserLevel();
-		if ( $bIgnoreLoggedInUser && $nCurrentUserLevel >= 0 ) { // logged in
+		if ( ( $aOpts[ 'ignore_logged_in_user' ] == 'Y' ) && $nCurrentUserLevel >= 0 ) { // logged in
 			$nIgnoreFromUserLevel = $aOpts[ 'ignore_from_user_level' ];
 			if ( $nCurrentUserLevel >= $nIgnoreFromUserLevel ) {
-				return true;
+				$bIgnore = true;
 			}
 		}
-		return false;
+
+		return $bIgnore;
 	}
 
 	/**
@@ -105,10 +104,10 @@ class ICWP_APP_Processor_GoogleAnalytics extends ICWP_APP_Processor_BaseApp {
 	private function getAnalyticsCode_Classic() {
 		$aOpts = $this->getGaOpts();
 		$sRaw = "
-				<!-- Google Analytics Tracking by iControlWP -->
+				<!-- Google Analytics by iControlWP -->
 				<script type=\"text/javascript\">//<![CDATA[
 					var _gaq=_gaq||[];
-					_gaq.push(['_setAccount','{{CODE}}']);
+					_gaq.push(['_setAccount','{{TRACKING_ID}}']);
 					_gaq.push(['_trackPageview']);
 					( function() {
 						var ga=document.createElement('script');
@@ -120,7 +119,7 @@ class ICWP_APP_Processor_GoogleAnalytics extends ICWP_APP_Processor_BaseApp {
 					})();
 				 //]]></script>
 			";
-		return str_replace( '{{CODE}}', $aOpts[ 'tracking_id' ], $sRaw );
+		return str_replace( '{{TRACKING_ID}}', $aOpts[ 'tracking_id' ], $sRaw );
 	}
 
 	/**
@@ -136,10 +135,10 @@ class ICWP_APP_Processor_GoogleAnalytics extends ICWP_APP_Processor_BaseApp {
 				var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
 				j.async=true;
 				j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-			})(window,document,'script','dataLayer','{{CODE}}');</script>
+			})(window,document,'script','dataLayer','{{TRACKING_ID}}');</script>
 			<!-- End Google Tag Manager -->
 		";
-		return str_replace( '{{CODE}}', $aOpts[ 'tracking_id' ], $sRaw );
+		return str_replace( '{{TRACKING_ID}}', $aOpts[ 'tracking_id' ], $sRaw );
 	}
 
 	/**
@@ -148,11 +147,11 @@ class ICWP_APP_Processor_GoogleAnalytics extends ICWP_APP_Processor_BaseApp {
 		$aOpts = $this->getGaOpts();
 		$sRaw = '
 			<!-- Google Tag Manager (noscript) by iControlWP -->
-			<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{CODE}}"
+			<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{TRACKING_ID}}"
 			height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 			<!-- End Google Tag Manager (noscript) -->
 		';
-		echo str_replace( '{{CODE}}', $aOpts[ 'tracking_id' ], $sRaw );
+		echo str_replace( '{{TRACKING_ID}}', $aOpts[ 'tracking_id' ], $sRaw );
 	}
 
 	/**
@@ -161,19 +160,19 @@ class ICWP_APP_Processor_GoogleAnalytics extends ICWP_APP_Processor_BaseApp {
 	public function getAnalyticsCode_Universal() {
 		$aOpts = $this->getGaOpts();
 		$sRaw = "
-				<!-- Google Analytics Tracking by iControlWP -->
+				<!-- Google Analytics (Universal) by iControlWP -->
 				<script type=\"text/javascript\">//<![CDATA[
 				  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 				  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 				  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 				  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-				  ga('create', '{{CODE}}', 'auto');
+				  ga('create', '{{TRACKING_ID}}', 'auto');
 				  ga('require', 'displayfeatures');
 				  ga('send', 'pageview');
 				 //]]></script>
 			";
-		return str_replace( '{{CODE}}', $aOpts[ 'tracking_id' ], $sRaw );
+		return str_replace( '{{TRACKING_ID}}', $aOpts[ 'tracking_id' ], $sRaw );
 	}
 
 	/**
